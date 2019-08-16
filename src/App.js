8de8ce20158250d646523e2f5701f0a7f9b6e44e
DDevinda <dushandevinda94@gmail.com>
2019-08-16 12:31:42 +0530

    adding file upload front end

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import UploadForm from './components/form';
+import Heading from './components/heading';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Heading />
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
